62e137ba7ef6a53ea108bd308e1d69ba369d715e
kadirzbk <147185472+kadirzbk@users.noreply.github.com>
2024-10-17 21:07:08 +0300

    Create veriYapilari1.c

---
+++ veriYapilari1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct dugum {
+    int veri;
+    struct dugum* gosterici;
+};
+
+int main() {
+    struct dugum* bir;
+    struct dugum* iki;
+    struct dugum* uc;
+    struct dugum* dort;
+
+    bir = (struct dugum*) malloc(sizeof(struct dugum));
+    iki = (struct dugum*) malloc(sizeof(struct dugum));
+    uc = (struct dugum*) malloc(sizeof(struct dugum));
+    dort = (struct dugum*) malloc(sizeof(struct dugum));
+
+    bir->gosterici = iki;
+    bir->veri = 11;
+
+    iki->gosterici = uc;
+    iki->veri = 22;
+
+    uc->gosterici = dort;
+    uc->veri = 33;
+
+    dort->gosterici = NULL;
+    dort->veri = 44;
+
+    printf("%d %d %d %d\n", bir->veri, iki->veri, uc->veri, dort->veri);
+
+    return 0;
+}
