4f1f0777be0e4c1321e2dd4e7a78e6fb9199aa2d
kadirzbk <147185472+kadirzbk@users.noreply.github.com>
2024-10-17 21:35:57 +0300

    Create veriYapilari9.c

---
+++ veriYapilari9.c
@@ -0,0 +1,244 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *start = NULL;
+struct node *temp = NULL;
+
+void basaEkle(int veri) {
+    struct node *eleman;
+    eleman = (struct node *) malloc(sizeof(struct node));
+    eleman->data = veri;
+    eleman->next = start;
+    start = eleman;
+}
+
+void sonaEkle(int veri) {
+    struct node *eleman;
+    eleman = (struct node *) malloc(sizeof(struct node));
+    eleman->data = veri;
+    eleman->next = NULL;
+
+    if (start == NULL) {
+        start = eleman;
+    } else {
+        temp = start;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = eleman;
+    }
+}
+
+void arayaEkle(int n, int veri) {
+    struct node *eleman;
+    eleman = (struct node *) malloc(sizeof(struct node));
+    eleman->data = veri;
+
+    temp = start;
+
+    
+    if (temp == NULL) {
+        printf("Liste bo�, araya eleman eklenemez.\n");
+        return;
+    }
+
+   
+    while (temp != NULL && temp->data != n) {
+        temp = temp->next;
+    }
+
+    
+    if (temp == NULL) {
+        printf("Eleman listede bulunamad�.\n");
+        free(eleman); 
+        return;
+    }
+
+   
+    eleman->next = temp->next;
+    temp->next = eleman;
+}
+
+void bastanSil()
+{
+	if(start!=NULL)
+	{
+	
+	temp=start->next;
+	free(start ) ;
+	start= temp;
+    } 
+    else 
+    free(start) ;
+
+}
+
+void sondanSil()
+{
+	if(start->next!= NULL )
+	{
+		temp= start;
+		while (temp->next->next!=NULL )
+		{
+			temp= temp->next;
+		}
+		free(temp->next ) ;
+		temp->next= NULL;
+		
+	}
+	else
+	bastanSil();
+	
+}
+
+void aradanSil(int veri)
+{
+	
+
+	temp= start;
+	if( temp->data==veri )
+	{
+		bastanSil();
+	}
+	else
+	{
+		struct node * temp2;
+		temp2= (struct node *) malloc (sizeof(struct node  )) ;
+		
+		while(temp->next->data!= veri)
+		{
+			temp= temp->next;
+			
+		}
+		if(temp->next->next == NULL )
+		{
+			sondanSil();
+		}
+		
+		else
+		{
+			temp2= temp->next->next;
+	     	free(temp->next);
+		    temp->next= temp2;
+		}
+		
+	}
+
+	
+	
+	
+}
+
+void tersCevir()
+{
+	struct node * temp2= NULL;
+	struct node *prev= NULL;
+	
+	temp= start;
+	 while(temp!= NULL )
+	{
+		temp2= temp->next;
+		temp->next= prev;
+		prev= temp;
+		temp= temp2;
+	}
+	start = prev;
+}
+
+
+
+
+void yazdir() 
+{
+	system("cls") ;
+	if(start!= NULL  )
+	{
+    temp = start;
+    while (temp->next!= NULL) 
+    
+	
+	{
+        printf("%d ", temp->data );
+        temp = temp->next;
+    } 
+	printf("%d ", temp->data );
+   
+    }
+    else
+    printf("Eleman yok ! !") ;
+}
+
+int main() {
+    int sayi, adet = 0, secim, n;
+
+    while (adet != 6) {
+        printf("\nBasa eleman eklemek icin 1\n");
+        printf("Sona eleman eklemek icin 2\n");
+        printf("Araya eleman eklemek icin 3\n");
+        printf("Bastan eleman silmek icin 4\n");
+        printf("Sondan eleman silmek icin 5\n");
+        printf("Aradan eleman silmek icin 6\n");
+        printf("Elamanlari ters cevirmek icin 7\n");
+        printf("Seciminizi yapiniz: ");
+        scanf("%d", &secim);
+
+        switch (secim) {
+            case 1:
+                printf("\nBasa eklenecek elemanin degeri: ");
+                scanf("%d", &sayi);
+                basaEkle(sayi);
+                yazdir();
+                break;
+
+            case 2:
+                printf("\nSona eklenecek elemanin degeri: ");
+                scanf("%d", &sayi);
+                sonaEkle(sayi);
+                yazdir();
+                break;
+
+            case 3:
+                printf("Hangi sayidan once araya eklemek istersiniz: ");
+                scanf("%d", &n);
+                printf("\nAraya eklenecek elemanin degeri: ");
+                scanf("%d", &sayi);
+                arayaEkle(n, sayi);
+                yazdir();
+                break;
+                
+            case 4:
+                bastanSil();
+                yazdir();
+                break;
+
+            case 5:
+                sondanSil();
+                yazdir();
+                break;
+                
+            case 6: printf("Aradan silinecek elemanin degeri : ") ;
+                scanf("%d" , &sayi) ;
+                aradanSil(sayi ) ;
+                yazdir() ;
+                break;
+                
+            case 7: printf("Elamanlar ters cevriliyor.... : \n") ;
+                tersCevir() ;
+                yazdir() ;
+                break;
+                
+                
+            default:
+                printf("Gecersiz secim!\n");
+                break;
+        }
+        adet++;
+    }
+
+    return 0;
+}
